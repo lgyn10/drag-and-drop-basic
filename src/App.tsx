d65dcf04d5005ecdb21fe8eb86307f49cc129df4
ChanGyunLee <lgyn10@naver.com>
2024-06-28 11:28:18 +0900

    feat: 기본적인 DnD 기능 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,61 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import { DragDropContext, Draggable, DropResult, Droppable } from 'react-beautiful-dnd';
+import './App.css';
+
+const finalSpaceCharacters = [
+  { id: 'gary', name: 'Gary Goodspeed' },
+  { id: 'cato', name: 'Little Cato' },
+  { id: 'kvn', name: 'KVN' },
+  { id: 'avocato', name: 'Avocato' },
+  { id: 'quinn', name: 'Quinn Ergon' },
+  { id: 'hue', name: 'H.U.E.' },
+  { id: 'mooncake', name: 'Mooncake' },
+];
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [characters, setCharacters] = useState(finalSpaceCharacters);
+  // result 매개변수에는 source 항목 및 대상 위치와 같은 드래그 이벤트에 대한 정보가 포함
+  const handleEnd = (result: DropResult) => {
+    // console.log(result);
+    // 목적지 없을 때, 함수 종료
+    if (!result.destination) return;
+    // console.log('출발지 idx', result.source.index);
+    // console.log('목적지 idx', result.destination.index);
+    const startIdx = result.source.index;
+    const endIdx = result.destination.index;
+    const tempArr = [...characters]; // array.from()도 얕은 복사
+    const movingElement = tempArr.splice(startIdx, 1)[0];
+    const editedTempArr = [...tempArr.slice(0, endIdx), movingElement, ...tempArr.slice(endIdx)];
+    setCharacters(editedTempArr);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className='App'>
+      <h1>Fimal Space Characters</h1>
+      <DragDropContext onDragEnd={handleEnd}>
+        <Droppable droppableId='characters'>
+          {(provided) => {
+            return (
+              <ul className='characters' {...provided.droppableProps} ref={provided.innerRef}>
+                {characters.map((item, idx) => (
+                  <Draggable key={item.id} draggableId={item.id} index={idx}>
+                    {(provided) => {
+                      return (
+                        <li ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+                          <p>{item.name}</p>
+                        </li>
+                      );
+                    }}
+                  </Draggable>
+                ))}
+                {provided.placeholder}
+              </ul>
+            );
+          }}
+        </Droppable>
+      </DragDropContext>
+    </div>
+  );
 }
 
-export default App
+export default App;
